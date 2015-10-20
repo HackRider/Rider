2c661f00b3f3a0e1451e2d436034b9e0b04a4b84
Robert Lynch <robertlynch2012@gmail.com>
2015-10-20 14:40:27 -0400

    Added Routing/ Rendering code

---
+++ src/Route/Render.php
@@ -0,0 +1,33 @@
+<?hh // strict
+
+namespace Rider\Route;
+
+use Rider\Controller\BaseController;
+use XHPRoot;
+
+class Render {
+  public static function go(BaseController $controller, string $method): void {
+    $content = call_user_func(array($controller, $method));
+    if(is_object($content) && is_a($content, \XHPRoot::class)) {
+      self::renderXHP($content, $controller);
+    } elseif (
+      (is_array($content)) ||
+      (is_object($content) && is_a($content, Map::class))
+    ) {
+      self::renderJSON($content);
+    }
+  }
+
+  private static function renderXHP(XHPRoot $content, BaseController $controller): void {
+    $title = $controller::getConfig()->getTitle();
+    print
+      <layout title={$title}>
+        {$content}
+      </layout>;
+  }
+
+  private static function renderJSON(mixed $content): void {
+    header('Content-Type: application/json');
+    print json_encode($content, JSON_PRETTY_PRINT);
+  }
+}
---
+++ src/Route/Router.php
@@ -0,0 +1,30 @@
+<?hh
+
+namespace Rider\Route;
+
+class Router {
+  public static function dispatch(string $path, string $method): void {
+
+    // Get the auto-generated URI Map
+    $routes = URIMap::getURIMap();
+
+    // Match the path
+    $uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+    foreach($routes as $route_path => $controller_name) {
+      if(preg_match(
+        "@^$route_path$@i",
+        "$uri",
+        $_SESSION['route_params'])
+      ) {
+        $controller = (new ReflectionClass($controller_name))->newInstance();
+        invariant($controller instanceof \Rider\Controller\BaseController, "Tried to instanciate a controller which does not extend Base Controller");
+
+        Auth::runChecks($controller->getConfig()->getChecks());
+
+        Render::go($controller, $method);
+
+        return;
+      }
+    }
+  }
+}
